81ae838f7f94a10c2fbf1ebecc93497bf6093b1d
Jukich <51247956+Jukich@users.noreply.github.com>
2020-02-18 15:28:21 +0200

    Add project files.

---
+++ CarServiceSystemWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CarServiceSystemWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
